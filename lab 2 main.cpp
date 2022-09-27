826a5b25be14407ff9a6e728568b5a9b59fe68e0
atmichael7 <50531800+atmichael7@users.noreply.github.com>
2022-09-26 22:26:37 -0700

    Update lab 2 main.cpp

--- lab 2 main.cpp
+++ lab 2 main.cpp
@@ -1,32 +1,45 @@
 // main cpp contains all the functions that the class will be using in the header file
 #include "sdisk.h"
 
+
 int Sdisk::getblock(int blocknumber, string& buffer){
+  fstream iofile;
+  iofile.open(diskname.c_str(), ios::in | ios::out);
   iofile.seekg(blocknumber * blocksize);
   buffer.clear();
   char c;
 
-  for (int i = 0; i < blocksize; i < blocksize; i++){
+  for (int i = 0; i < blocksize; i++){
     iofile.get(c);
     buffer.push_back(c);
-  }
+  } 
 }
 
 int Sdisk::putblock(int blocknumber, string buffer){
+  cout << "\nIN PUT\n";
+
   fstream iofile;
   iofile.open(diskname.c_str(), ios::in | ios::out);
   if (blocknumber < 0 || blocknumber >= numberofblocks){
     cout << "Block does not exist";
     return 0;
   }
-
+  //int blocksize = 32;
+  
   iofile.seekp(blocksize * blocknumber);
+  //char temp = 4;
   for (int i = 0; i < blocksize; i++){
-    iofile.put(buffer[i]); /////////// buff.at(i);
+    //temp = buffer.at(i);
+    //iofile.put(temp);
+    //iofile.put(buffer[i]); /////////// 
+    //iofile.put(buffer.at(i));
+    
   }
+
+  cout << "\nOUT PUT\n";
 }
 
-int Sdisk::Sdisk(string diskname, int numberofblocks, int blocksize){
+Sdisk::Sdisk(string diskname, int numberofblocks, int blocksize){
   this->diskname = diskname;
   this->numberofblocks = numberofblocks;
   this->blocksize = blocksize;
@@ -42,9 +55,17 @@ int Sdisk::Sdisk(string diskname, int numberofblocks, int blocksize){
   }
 
   if (counter == numberofblocks * blocksize)
-    return 1;
+    cout << "disk bad";
   else
     cout << "disk size good!";
+}
+
+int getnumberofblocks(){
+  
+} 
+
+int Sdisk::getblocksize(){
+  return blocksize;
 }
 
 
@@ -61,11 +82,15 @@ int main()
   disk1.getblock(4, block3);
   cout << "Should be 32 1's : ";
   cout << block3 << endl;
+
+  
+  /*
   disk1.putblock(8, block2);
   disk1.getblock(8, block4);
   cout << "Should be 32 2's : ";
   cout << block4 << endl;
-
+  cout << block1 << endl;
+  */
 
 
 }
